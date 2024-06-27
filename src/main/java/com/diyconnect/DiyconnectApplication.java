16e4d3c432d97a85ada50a5c1f585a65e0983234
abraham <abrahamsuarezolay@gmail.com>
2024-06-27 11:50:24 +0100

    First commit

---
+++ src/main/java/com/diyconnect/DiyconnectApplication.java
@@ -0,0 +1,13 @@
+package com.diyconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyconnectApplication.class, args);
+	}
+
+}
