aaaf8e538a5d7d97071cbcf25f86cbedc1934a13
abraham <abrahamsuarezolay@gmail.com>
2024-09-01 18:02:23 +0100

    Added all control for finding username and fetching its info on login.

--- src/main/java/com/diyconnect/security/AuthController.java
+++ src/main/java/com/diyconnect/security/AuthController.java
@@ -1,11 +1,15 @@
 package com.diyconnect.security;
 
+import com.diyconnect.user.User;
+import com.diyconnect.user.payload.UsernameDTO;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +20,7 @@ import static com.diyconnect.security.TokenJWTConfig.SECRET_KEY;
 @RequestMapping("/auth")
 public class AuthController {
     @GetMapping("/verifytoken")
-    public void verifyToken(HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity<?> verifyToken(HttpServletRequest request, HttpServletResponse response) {
         Cookie[] cookies = request.getCookies();
         String token = null;
 
@@ -33,16 +37,17 @@ public class AuthController {
             try {
                 // Parse and verify the token
                 Claims claims = Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(token).getPayload();
-
                 // If valid, return HTTP 200 OK
-                response.setStatus(HttpServletResponse.SC_OK);
+                UsernameDTO username = new UsernameDTO(claims.getSubject());
+
+                return new ResponseEntity<UsernameDTO>(username, HttpStatus.OK);
             } catch (JwtException e) {
                 // If invalid, return HTTP 401 Unauthorized
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return new ResponseEntity<>("Unauthorized token", HttpStatus.UNAUTHORIZED);
             }
         } else {
             // No token present, return HTTP 401 Unauthorized
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
         }
     }
 }
---
+++ src/main/java/com/diyconnect/user/payload/UsernameDTO.java
@@ -0,0 +1,12 @@
+package com.diyconnect.user.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsernameDTO {
+    private String username;
+}
